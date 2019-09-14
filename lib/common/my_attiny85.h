ffcf8f14d5f62d984ee018d08082b21c4ae9d20f
Daniel Garcia Briseno <dg.briseno@gmail.com>
2019-09-14 10:52:04 -0700

    Update to use predefined pin definitions

--- lib/common/my_attiny85.h
+++ lib/common/my_attiny85.h
@@ -3,18 +3,10 @@
  * the attiny85 using avr-libc
  */
 
-
-/**
- * Define bits for pins for easier referencing
- */
-#define PIN0 0b000001
-#define PIN1 0b000010
-#define PIN2 0b000100
-#define PIN3 0b001000
-#define PIN4 0b010000
-#define PIN5 0b100000
-
 /**
  * Toggles a pin bit in the given port.
+ *
+ * @param[in] port_val - PORTX
+ * @param[in] pin - pin to toggle, PINX
  */
-#define TOGGLE_PORT_PIN(port_val, pin) (port_val ^ pin)
+#define TOGGLE_PORT_PIN(port_val, pin) (port_val ^ (1 << pin))
--- src/blinky.c
+++ src/blinky.c
@@ -1,15 +1,16 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include "my_attiny85.h"
 
 int main(void)
 {
-    DDRB = 0xFF;
-    PORTB = 0XFF;
+    DDRB |= PIN0;  // set PIN 0 as an output
+    PORTB |= PIN0; // set PIN 0 output HIGH
 
     while (1) {
         _delay_ms(500);
-        PORTB = 0;
+        PORTB = TOGGLE_PORT_PIN(PORTB, PIN0);
         _delay_ms(500);
-        PORTB = 0xFF;
+        PORTB = TOGGLE_PORT_PIN(PORTB, PIN0);
     }
 }
